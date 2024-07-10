d0d9e2fe62291f859a8d9704ebfd9f5e31f70efe
Matt Fisher <mfisher87@gmail.com>
2024-07-10 09:54:56 -0600

    Extract Splash component from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import nsidcLogo from '@src/assets/nsidc-logo.svg'
+import Splash from '@src/Splash.tsx'
 import '@src/App.css'
 
 const App = () => (
   <>
-    <div>
-      <a href="https://nsidg.org" target="_blank" rel="noreferrer">
-        <img src={nsidcLogo} className="logo nsidc" alt="NSIDC logo" />
-      </a>
-    </div>
-    <h1>Arctic Rain on Snow Study (AROSS) - Automated Surface Observation Station (ASOS) Events Database</h1>
+    <Splash />
   </>
 );
 
---
+++ src/Splash.tsx
@@ -0,0 +1,17 @@
+import nsidcLogo from '@src/assets/nsidc-logo.svg'
+
+const Splash = () => (
+  <>
+    <div>
+      <a href="https://nsidg.org" target="_blank" rel="noreferrer">
+        <img src={nsidcLogo} className="logo nsidc" alt="NSIDC logo" />
+      </a>
+    </div>
+    <h1>
+      Arctic Rain on Snow Study (AROSS) -
+      Automated Surface Observation Station (ASOS) Events Database
+    </h1>
+  </>
+);
+
+export default Splash;
