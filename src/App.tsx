637861a9dabab708de856efb1381c8d912394242
Matt Fisher <mfisher87@gmail.com>
2024-07-09 20:05:16 -0600

    Fix linting errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import nsidcLogo from '@src/assets/nsidc-logo.svg'
 import '@src/App.css'
 
-function App() {
-  return (
-    <>
-      <div>
-        <a href="https://nsidg.org" target="_blank">
-          <img src={nsidcLogo} className="logo nsidc" alt="NSIDC logo" />
-        </a>
-      </div>
-      <h1>Arctic Rain on Snow Study (AROSS) - Automated Surface Observation Station (ASOS) Events Database</h1>
-    </>
-  )
-}
+const App = () => (
+  <>
+    <div>
+      <a href="https://nsidg.org" target="_blank" rel="noreferrer">
+        <img src={nsidcLogo} className="logo nsidc" alt="NSIDC logo" />
+      </a>
+    </div>
+    <h1>Arctic Rain on Snow Study (AROSS) - Automated Surface Observation Station (ASOS) Events Database</h1>
+  </>
+);
 
 export default App
